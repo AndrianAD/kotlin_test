78f475f9e7b1e83fbc258cde784b217e224d9d41
Tegritee <Tegritee@tutanota.com>
2019-10-23 17:27:05 +0300

    clear code3

--- app/src/main/java/com/example/kotlin_test/Fragments/ScaleFragment.kt
+++ app/src/main/java/com/example/kotlin_test/Fragments/ScaleFragment.kt
@@ -2,25 +2,11 @@ package com.example.kotlin_test.Fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
 import com.example.kotlin_test.R
-import kotlinx.android.synthetic.main.fragment_scale.*
-import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class ScaleFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -28,8 +14,6 @@ class ScaleFragment : androidx.fragment.app.Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scale, container, false)
     }
-
-
 
 
 }
